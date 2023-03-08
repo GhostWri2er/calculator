5977a2578a34a65952b0fae56f0bc83c20bdd296
Ivan Mironovich <ivan.mironovich97@yandex.ru>
2023-03-08 12:15:30 +0500

    feat: add app

---
+++ calculator/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="content">
+        <div className="content__block">
+          <input className="content__input"></input>
+        </div>
+        <div className="content__buttons">
+          <button className="content__button">AC</button>
+          <button className="content__button">+/-</button>
+          <button className="content__button">%</button>
+          <button className="content__button">/</button>
+          <button className="content__button">7</button>
+          <button className="content__button">8</button>
+          <button className="content__button">9</button>
+          <button className="content__button">x</button>
+          <button className="content__button">4</button>
+          <button className="content__button">5</button>
+          <button className="content__button">6</button>
+          <button className="content__button">-</button>
+          <button className="content__button">1</button>
+          <button className="content__button">2</button>
+          <button className="content__button">3</button>
+          <button className="content__button">+</button>
+          <button className="content__button">0</button>
+          <button className="content__button">.</button>
+          <button className="content__button">=</button>
+        </div>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
